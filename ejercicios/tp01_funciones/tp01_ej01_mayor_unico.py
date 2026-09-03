a266466d8e1cda51f60dd767b31a08bc527429dd
erikafcf-punto <erikafcf0@gmail.com>
2026-09-03 19:20:24 -0300

    primer ejercicio

---
+++ ejercicios/tp01_funciones/tp01_ej01_mayor_unico.py
@@ -0,0 +1,52 @@
+def mayor_unico(num1,num2,num3):
+    """
+    Compara tres números enteros positivos  y determina si existe un unico mayor.
+    pre: recibe 3 números positivos
+    post: Devuelve el entero con mayor valor o un -1 cuando no exista un solo mayor.
+
+    """
+    if num1 > num2:
+        if num1> num3:
+            return num1
+
+        elif num1 < num3:
+            return num3
+
+        else:
+            return -1
+
+    elif num1 < num2:
+        if num2 > num3:
+            return num2
+        elif num2 < num3:
+            return num3
+        else:
+            return -1
+
+    elif num1 == num2:
+        if num1 > num3:
+            return -1
+        elif num3 > num1:
+            return num3
+        else :
+            return -1
+
+
+def main():
+
+    num1= 0
+    while num1 <= 0 :
+        num1 = int(input("ingrese el numero a comparar: "))   
+
+    num2= 0
+    while num2 <= 0 :
+        num2 = int(input("ingrese el numero a comparar: "))        
+
+    num3= 0
+    while num3 <= 0 :
+        num3 = int(input("ingrese el numero a comparar: "))
+
+
+    print(mayor_unico(num1,num2,num3))
+
+main()
